2724aed3ead7e83d9af0a72e117b9962fc6ece74
ChindanaiNaKub <ChindanaiNaKub@users.noreply.github.com>
2025-08-09 09:26:08 +0000

    deploy: 77b5abe5d64333a74198943e804b137b27cefca4

--- _next/static/chunks/app/layout-a286120740f3546a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{19324:()=>{},47561:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,65299,23)),Promise.resolve().then(l.t.bind(l,19324,23))},65299:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_3be413"}}},e=>{var s=s=>e(e.s=s);e.O(0,[618,441,684,358],()=>s(47561)),_N_E=e.O()}]);
